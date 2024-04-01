fadf5c88d9a016d70173e355d6eb6c8f8165a2ed
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-04-01 20:56:27 +0300

    Add files via upload

---
+++ Reflection and Attributes/Validation Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public  class MyRequiredAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj)
+        {
+            var isValid = obj != null;
+            return isValid;
+        }
+    }
+}
---
+++ Reflection and Attributes/Validation Attributes/Validator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            foreach (var prop in properties)
+            {
+                var atribute = prop.GetCustomAttributes()
+                    .Where(x => x.GetType().IsSubclassOf(typeof(MyValidationAttribute)))
+                   .ToArray();
+
+                foreach (MyValidationAttribute atr in atribute)
+                {
+                    bool isVALID = atr.IsValid(prop.GetValue(obj));
+                    if (!isVALID)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
