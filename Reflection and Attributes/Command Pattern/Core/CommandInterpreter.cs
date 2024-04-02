0db37e2a30655a618270ba94895f525a5489828d
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-04-02 20:54:55 +0300

    Add files via upload

---
+++ Reflection and Attributes/Command Pattern/Core/CommandInterpreter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+
+        public string Read(string args)
+        {
+            string[] inputinfo = args.Split();
+
+            string comandName = inputinfo[0] + "Command";
+            string[] param = inputinfo.Skip(1).ToArray();
+            string result = String.Empty;
+
+
+            Type Type = Assembly.GetCallingAssembly().GetTypes().Where(x => x.Name == comandName).FirstOrDefault();
+           
+            if (Type==null)
+            {
+                throw new InvalidOperationException("Invalid command");
+            }
+            ICommand command = Activator.CreateInstance(Type) as ICommand;
+
+            result = command.Execute(param);
+            return result;
+        }
+    }
+}
---
+++ Reflection and Attributes/Command Pattern/Core/Engine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreterer)
+        {
+            this.commandInterpreter = commandInterpreterer;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+
+                    string result = this.commandInterpreter.Read(input);
+
+                    Console.WriteLine(result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+        }
+    }
+}
