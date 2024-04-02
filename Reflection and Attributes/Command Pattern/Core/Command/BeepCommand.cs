82fb06204156e80eb546c4b52f6206990c715c2f
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-04-02 20:56:13 +0300

    Add files via upload

---
+++ Reflection and Attributes/Command Pattern/Core/Command/BeepCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class BeepCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"Beep, {args[0]}";
+        }
+    }
+}
---
+++ Reflection and Attributes/Command Pattern/Core/Command/ComplexCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class ComplexCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"ComplexCommand, {args[0]}";
+        }
+    }
+}
